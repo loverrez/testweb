ab581f761d74ca585d8fea0443a4e1627b156690
Thanathip <thanathip.lumlert2@gmail.com>
2026-03-04 06:48:46 +0700

    อัพเดทใหม่

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function LandingPage() {
   return (
-    <div className="min-h-[90vh] flex items-center justify-center px-4">
-      <div className="relative w-full h-[80vh] max-w-7xl">
+    <div className="min-h-[90vh] flex items-start justify-center px-2 pt-24">
+      <div className="relative w-full h-[70vh] max-w-[98vw]">
         {/* Empty Frame - Transparent background, can see through to website background */}
         <div className="relative w-full h-full bg-transparent border border-white/20 rounded-3xl">
           {/* Empty content - just the frame */}
