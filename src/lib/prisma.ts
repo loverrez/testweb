18c88272a6e732ac3315a1c1c63d593a4b1722fa
Thanathip <thanathip.lumlert2@gmail.com>
2026-03-04 02:01:40 +0700

    อัพเดทใหม่

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  // @ts-ignore
+  // @ts-expect-error datasourceUrl is set at runtime
   datasourceUrl: process.env.DATABASE_URL
 })
 
