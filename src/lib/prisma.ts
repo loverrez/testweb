ee76b95b5070297fe2afe81bdd10ae6a5ad88267
Thanathip <thanathip.lumlert2@gmail.com>
2026-03-04 00:18:14 +0700

    อัพเดทใหม่

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
+  // @ts-ignore
   datasourceUrl: process.env.DATABASE_URL
 })
 
