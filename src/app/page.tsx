af341c7d517e5c3ebb589eba03b00dd05d976c60
Thanathip <thanathip.lumlert2@gmail.com>
2026-03-04 01:38:55 +0700

    อัพเดทใหม่

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
-      <div className="mt-12 flex gap-4">
-        <div className="w-12 h-12 rounded-full border border-red-900/50 bg-zinc-950 flex items-center justify-center box-red-glow animate-bounce">
-          <div className="w-2 h-2 bg-red-600 rounded-full"></div>
-        </div>
-      </div>
     </div>
   );
 }
